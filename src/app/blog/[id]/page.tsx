cc18d1eb9a80ba7856803bc13f2cb362a90c439b
hiroshi-hirobe <hiroshi.hirobe@esm.co.jp>
2025-10-26 11:53:46 +0900

    ルーティングの基本（静的・動的）

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Params = {
+  params: Promise<{
+    id: string
+  }>
+}
+
+export default async function BlogPage({ params }: Params) {
+
+  const { id } = await params
+  
+  return (
+    <div>
+      ブログID: { id }
+    </div>
+  )
+}
