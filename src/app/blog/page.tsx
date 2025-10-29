caccce3ae3624d0012fb47e61e610a59f57243d7
hiroshi-hirobe <hiroshi.hirobe@esm.co.jp>
2025-10-29 21:55:40 +0900

    特別なページの描画

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,31 @@
+// ダミーデータ
+const articles = [
+  {id: "1", title: "タイトル1"},
+  {id: "2", title: "タイトル2"},
+  {id: "3", title: "タイトル3"},
+]
+
+// 3秒間待機
+async function fetchArtiles() {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  // // エラーページの確認用
+  // throw new Error('エラーが発生')
+
+  return articles
+}
+
+export default async function BlogPage() {
+  const articles = await fetchArtiles()
+  return (
+    <div>
+      <ul>
+        { articles.map((article) => (
+          <li key={article.id}>
+            title: {article.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
