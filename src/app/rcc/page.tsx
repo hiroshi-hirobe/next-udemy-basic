598173411cddcfe0e7b40b438ad0f7e6bc052232
hiroshi-hirobe <hiroshi.hirobe@esm.co.jp>
2025-11-17 22:05:49 +0900

    リンクの設定方法

--- src/app/rcc/page.tsx
+++ src/app/rcc/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import { useState } from "react"
+import { useRouter } from "next/navigation"  // 同名のuseRouterが過去に提供されていたため、インポート元に注意
+import Link from "next/link"
 
 export default function ClientComponent() {
   const [ count, setCount ] = useState(0)
+  const router = useRouter()
 
   console.log('Client')
   return (
     <div>
       クライアント
       <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+      <Link href='/about'>About</Link>
+      <button onClick={() => router.push('/about')}>About</button> 
     </div>
   )
 }
