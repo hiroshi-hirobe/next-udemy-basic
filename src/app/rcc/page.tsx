9293b9939b17076f84e1f0f6e3f302141714465c
hiroshi-hirobe <hiroshi.hirobe@esm.co.jp>
2025-11-17 21:39:19 +0900

    クライアントコンポーネントの記法

---
+++ src/app/rcc/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function ClientComponent() {
+  console.log('Client')
+  return (
+    <div>
+      クライアント
+    </div>
+  )
+}
---
+++ src/app/rsc/page.tsx
@@ -0,0 +1,8 @@
+export default function ServerComponent() {
+  console.log('Sever')
+  return (
+    <div>
+      サーバー
+    </div>
+  )
+}
