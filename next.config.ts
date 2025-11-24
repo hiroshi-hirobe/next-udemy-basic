9aa4de3c6827b7333c55d2e1cefe2a1e51fc51d7
hiroshi-hirobe <hiroshi.hirobe@esm.co.jp>
2025-11-24 17:19:58 +0900

    3種類のレンダリング方法

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // 外部APIの画像使用時は外部ドメインを許可する
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.dog.ceo',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
